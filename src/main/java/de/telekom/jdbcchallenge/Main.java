dda75ad725d160bbf8496fcad64ec0543ad1e5c4
Gerd Barodte <achsch@gmail.com>
2021-05-18 15:10:17 +0200

    Integration maven-assembly-plugin in pom.xml

--- src/main/java/de/telekom/jdbcchallenge/Main.java
+++ src/main/java/de/telekom/jdbcchallenge/Main.java
@@ -12,27 +12,25 @@ public class Main {
 		final String DRIVER = "org.mariadb.jdbc.Driver";
 		Class.forName(DRIVER);
 		try {
-			Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/seadb?user=seauser&password=seapass");
+			Connection connection = DriverManager
+					.getConnection("jdbc:mariadb://localhost:3306/seadb?user=seauser&password=seapass");
 			Statement statement = connection.createStatement();
-	//		ResultSet resultSet = statement.executeQuery( "select count(*) from personen");
 
-	//		System.out.println("Es gibt " + resultSet.getInt(1) + " Datensätze");
+			ResultSet resultSet = statement.executeQuery("select * from personen");
 
-	//		ResultSet resultSet = statement.executeQuery( "select * from personen");
-			
-			ResultSet resultSet = statement.executeQuery( "select * from personen");
-			
-			while(resultSet.next()) {
-		        System.out.println("ID       : " + resultSet.getInt(1));    // ID
-		        System.out.println("Anrede   : " + resultSet.getInt(2));    // Anrede
-	     	    System.out.println("Vorname  : " + resultSet.getString(3)); // Vorname
-	     	    System.out.println("Nachname : " + resultSet.getString(4)); // Nachname
-	     	    System.out.println("");                                     // Leer
+			while (resultSet.next()) {
+				System.out.println("ID       : " + resultSet.getInt(1)); // ID
+				System.out.println("Anrede   : " + resultSet.getInt(2)); // Anrede
+				System.out.println("Vorname  : " + resultSet.getString(3)); // Vorname
+				System.out.println("Nachname : " + resultSet.getString(4)); // Nachname
+				System.out.println(""); // Leer
 			}
-				
-						
+
 			connection.close();
-		} catch (Exception e) { System.out.println("Ausgabe");}
+		} catch (Exception e) {
+			System.out.println("Ausgabe");
+		}
+
 	}
 
 }
