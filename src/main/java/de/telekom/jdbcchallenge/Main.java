76d22dedfaa5a3441b4452d9331d1616639443f9
Gerd Barodte <achsch@gmail.com>
2021-05-18 14:41:39 +0200

    Datenbankzugriff mit Schleife und Ausgabe

--- src/main/java/de/telekom/jdbcchallenge/Main.java
+++ src/main/java/de/telekom/jdbcchallenge/Main.java
@@ -1,7 +1,9 @@
 package de.telekom.jdbcchallenge;
 
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 
 public class Main {
 
@@ -10,10 +12,27 @@ public class Main {
 		final String DRIVER = "org.mariadb.jdbc.Driver";
 		Class.forName(DRIVER);
 		try {
-			Connection connection = DriverManager
-					.getConnection("jdbc:mariadb://localhost:3306/seadb?user=seauser&password=seapass");
+			Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/seadb?user=seauser&password=seapass");
+			Statement statement = connection.createStatement();
+	//		ResultSet resultSet = statement.executeQuery( "select count(*) from personen");
+
+	//		System.out.println("Es gibt " + resultSet.getInt(1) + " Datensätze");
+
+	//		ResultSet resultSet = statement.executeQuery( "select * from personen");
+			
+			ResultSet resultSet = statement.executeQuery( "select * from personen");
+			
+			while(resultSet.next()) {
+		        System.out.println("ID       : " + resultSet.getInt(1));    // ID
+		        System.out.println("Anrede   : " + resultSet.getInt(2));    // Anrede
+	     	    System.out.println("Vorname  : " + resultSet.getString(3)); // Vorname
+	     	    System.out.println("Nachname : " + resultSet.getString(4)); // Nachname
+	     	    System.out.println("");                                     // Leer
+			}
+				
+						
 			connection.close();
-		} catch (Exception e) {}
+		} catch (Exception e) { System.out.println("Ausgabe");}
 	}
 
 }
